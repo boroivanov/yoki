18aa5c9b15430a0ad513dba4418fff1c886161e9
Borislav Ivanov <borogl@gmail.com>
2019-04-04 13:46:06 -0700

    add deployment list resource

--- resources/deployment.py
+++ resources/deployment.py
@@ -1,6 +1,22 @@
+import boto3
 from flask_restful import Resource
+from boto3.dynamodb.conditions import Key
 
 
 class DeploymentList(Resource):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('ECSDeployments')
+
     def get(self, cluster=None, service=None):
-        return 'hi there'
+        params = {
+            'IndexName': 'cluster-service-index',
+        }
+        if cluster and service:
+            params['KeyConditionExpression'] = Key('cluster').eq(
+                cluster) & Key('service').eq(service)
+        elif cluster and not service:
+            params['KeyConditionExpression'] = Key('cluster').eq(cluster)
+        else:
+            return self.table.scan()['Items']
+
+        return {'deployments': self.table.query(**params)['Items']}
