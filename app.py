47c0bdd35ec69309f4f30627411ee13c343edf75
Borislav Ivanov <borogl@gmail.com>
2019-04-04 11:26:34 -0700

    add deployment list routes

--- app.py
+++ app.py
@@ -6,7 +6,12 @@ from resources.deployment import DeploymentList
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(DeploymentList, '/')
+DeploymentList_routes = [
+    '/deployments',
+    '/clusters/<string:cluster>/deployments',
+    '/clusters/<string:cluster>/services/<string:service>/deployments',
+]
+api.add_resource(DeploymentList, *DeploymentList_routes)
 
 
 if __name__ == "__main__":
--- resources/deployment.py
+++ resources/deployment.py
@@ -2,5 +2,5 @@ from flask_restful import Resource
 
 
 class DeploymentList(Resource):
-    def get(self):
+    def get(self, cluster=None, service=None):
         return 'hi there'
