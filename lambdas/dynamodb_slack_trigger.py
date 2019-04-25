b099a0f3c86bbb670fb0e684edc1161fc5f60565
Borislav Ivanov <borogl@gmail.com>
2019-04-25 08:51:15 -0700

    add lambda for dynamodb slack trigger

--- lambdas/dynamodb_slack_trigger.py
+++ lambdas/dynamodb_slack_trigger.py
@@ -2,11 +2,22 @@ import os
 import json
 import logging
 
+from lib.slack import SlackTaskDigest
 
 log = logging.getLogger()
 log.setLevel(os.getenv('LOG_LEVEL', logging.INFO))
+
+SLACK_TOKEN = os.environ.get('SLACK_TOKEN', '')
+SLACK_CHANNEL = os.environ.get('SLACK_CHANNEL', 'general')
 
 
 def lambda_handler(event, context):
     log.info('Here is the event:')
     log.info(json.dumps(event))
+
+    if event['Records'][0]['eventName'] == 'REMOVE':
+        log.info('Skipping REMOVE event.')
+        return
+
+    digest = SlackTaskDigest(SLACK_TOKEN, SLACK_CHANNEL, event)
+    digest.post_to_slack()
