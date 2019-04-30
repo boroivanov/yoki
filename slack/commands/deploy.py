5b3a6970d6f887f42cfcfde8a0bb727c8e9751cc
Borislav Ivanov <borogl@gmail.com>
2019-04-30 14:47:30 -0700

    init version of slack deploy command

---
+++ slack/commands/deploy.py
@@ -0,0 +1,44 @@
+import os
+import requests
+
+
+YOKI_API = os.environ('YOKI_API')
+
+
+class SlackCommand(object):
+    def parse_args(self, args):
+        try:
+            self.cluster = args[0]
+            self.service = args[1]
+            tags = args[2:]
+            if len(tags) == 0:
+                raise IndexError
+
+            self.tags = self.parse_tags(tags)
+        except IndexError:
+            raise
+
+    def parse_tags(self, tags):
+        parsed = {}
+        for e, tag in enumerate(tags):
+            try:
+                container, tag = tag.split(':')
+            except ValueError:
+                container = e
+                tag = tag
+            parsed[container] = tag
+        return parsed
+
+    def run(self, args):
+        try:
+            self.parse_args(args)
+        except IndexError:
+            return self.help()
+
+        url = f'{YOKI_API}/clusters/{self.cluster}/services/{self.service}'
+        r = requests.post(url, json={'tags': self.tags})
+
+        return {'text': f'{r.text}'}
+
+    def help(self):
+        return {'text': 'Usage: deploy [cluster] [service] [tags]...'}
