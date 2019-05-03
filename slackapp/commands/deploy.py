b5fb661236fc9d6d2a7b5e1fb867ad68be247bea
Borislav Ivanov <borogl@gmail.com>
2019-05-03 15:47:50 -0700

    update deploy command help

--- slackapp/commands/deploy.py
+++ slackapp/commands/deploy.py
@@ -57,4 +57,17 @@ class SlackCommand(object):
         item.put_item()
 
     def help(self):
-        return {'text': 'Usage: deploy [cluster] [service] [tags]...'}
+        help_text = 'Usage: `deploy [cluster] [service] [tags]...`\n'
+        examples_text = 'Examples:\n' \
+            '`deploy cluster1 srv1 tag1` - deploy a single positional ' \
+            'tag. The first container in the service is going to be ' \
+            'updated.\n' \
+            '`deploy cluster1 srv1 tag1 tag2` - deploy two positional ' \
+            'containers. The first and second containers will be updated.\n' \
+            '`deploy cluster1 srv1 container1:tag1 container2:tag2` - ' \
+            'deploy two containers by name. Useful if you want to be ' \
+            'explicit ' \
+            'and/or you need to deploy to a specific container(s) in a ' \
+            'a service without updating the rest of the containers.'
+
+        return {'attachments': [{'text': help_text}, {'text': examples_text}]}
