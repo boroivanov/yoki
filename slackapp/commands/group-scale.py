72567281b97b9dbd0682edb05da689dc068348ea
Borislav Ivanov <borogl@gmail.com>
2019-05-09 09:45:42 -0700

    add group scale slack cmd

--- slackapp/commands/group-scale.py
+++ slackapp/commands/group-scale.py
@@ -2,6 +2,7 @@ import ast
 import os
 import requests
 
+from resources.group import ServiceGroup
 from models.notifications import Notifications
 
 
@@ -12,19 +13,39 @@ class SlackCommand(object):
 
     def run(self, args, params):
         try:
-            cluster, service, count = args
+            cluster, group, count = args
             count = int(count)
         except ValueError:
             return self.help()
 
-        url = f'{YOKI_API}/clusters/{cluster}/services/{service}/scale'
+        sg = ServiceGroup()
+        res = sg.get(group)
+
+        if isinstance(res, tuple):
+            return {'text': res[0]}
+
+        services = res['serviceGroup']['services'][0].split()
+
+        messages = []
+        for service in services:
+            msg = self.scale_service(cluster, group, count, params)
+            messages.append(msg)
+
+        attachments = []
+        for msg in messages:
+            attachments.append({'text': msg['message']})
+
+        return {'attachments': attachments}
+
+    def scale_service(self, cluster, group, count, params):
+        url = f'{YOKI_API}/clusters/{cluster}/groups/{group}/scale'
         r = requests.post(url, json={'count': count})
         data = ast.literal_eval(r.text)
 
         if 'deployment_id' in data:
             self.save_cmd_details(data, params)
 
-        return {'response_type': 'ephemeral', 'text': data['message']}
+        return data
 
     def save_cmd_details(self, data, params):
         deployment_id = data['deployment_id']
@@ -36,6 +57,6 @@ class SlackCommand(object):
         item.put_item()
 
     def help(self):
-        help_text = 'Usage: `scale [cluster] [service] [count]`\n'
+        help_text = 'Usage: `scale [cluster] [group] [count]`\n'
 
         return {'attachments': [{'text': help_text}]}
