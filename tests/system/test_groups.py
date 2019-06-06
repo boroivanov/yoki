37ff99730b4f6bb8af88c0bd56bda5786985760a
Borislav Ivanov <borogl@gmail.com>
2019-06-06 16:42:38 -0700

    add tests for service group deploy

--- tests/system/test_groups.py
+++ tests/system/test_groups.py
@@ -62,21 +62,107 @@ class TestServiceGroupList(TestMixin):
 
 
 class TestServiceGroupDeploy(TestMixin):
+    def create_test_group(self):
+        self.client.post(url_for('api.ServiceGroup', group='group1'),
+                         json={'services': 'srv1 srv2'},
+                         headers={'X-Yoki-Authorization': self.auth_header})
+
     def test_create_deployment_unauthorized(self):
         r = self.client.post(url_for('api.ServiceGroupDeploy',
-                                     cluster='stage', group='octo'))
+                                     cluster='test', group='group1'))
         assert r.status_code == 401
         assert 'Authorization Required' in str(r.data)
 
     def test_create_deployment(self):
+        self.create_test_group()
+
         url = url_for('api.ServiceGroupDeploy',
-                      cluster='stage', group='octo')
+                      cluster='test-yoki', group='group1')
 
         r = self.client.post(url,
-                             json={'tags': {'octo': 'awscp-5513185'}},
+                             json={'tags': {'container1': 'srv1'}},
+                             headers={
+                                 'X-Yoki-Authorization': self.auth_header})
+        assert r.status_code == 200
+
+        d = ast.literal_eval(r.data.decode('utf-8'))
+        msg1 = "Deploying to test-yoki srv1 " \
+            "with {'tags': {'container1': 'srv1'}}"
+        msg2 = "Deploying to test-yoki srv2 " \
+            "with {'tags': {'container1': 'srv1'}}"
+
+        assert 'deployment_id' in d['messages'][0]
+        assert 'deployment_id' in d['messages'][1]
+        assert msg1 == d['messages'][0]['message'] or \
+            msg1 == d['messages'][1]['message']
+        assert msg2 == d['messages'][0]['message'] or \
+            msg2 == d['messages'][1]['message']
+
+    def test_service_group_not_found(self):
+        url = url_for('api.ServiceGroupDeploy',
+                      cluster='test-yoki', group='missing')
+
+        r = self.client.post(url,
+                             json={'tags': {'container1': 'srv1'}},
                              headers={
                                  'X-Yoki-Authorization': self.auth_header})
         assert r.status_code == 200
         d = ast.literal_eval(r.data.decode('utf-8'))
-        assert d['message'] == "Deploying to stage hubble with " \
-            "{'tags': {'octo': 'awscp-5513185'}}"
+        assert d == {'message': 'Group not found: missing'}
+
+    def test_cluster_not_found(self):
+        self.create_test_group()
+
+        url = url_for('api.ServiceGroupDeploy',
+                      cluster='missing', group='group1')
+
+        r = self.client.post(url,
+                             json={'tags': {'container1': 'srv1'}},
+                             headers={
+                                 'X-Yoki-Authorization': self.auth_header})
+        assert r.status_code == 200
+
+        d = ast.literal_eval(r.data.decode('utf-8'))
+        msg1 = '[missing srv1]: Cluster not found: missing'
+        msg2 = '[missing srv2]: Cluster not found: missing'
+
+        assert msg1 == d['messages'][0]['message'] or \
+            msg1 == d['messages'][1]['message']
+        assert msg2 == d['messages'][0]['message'] or \
+            msg2 == d['messages'][1]['message']
+
+    def test_container_not_found(self):
+        self.create_test_group()
+
+        url = url_for('api.ServiceGroupDeploy',
+                      cluster='test-yoki', group='group1')
+
+        r = self.client.post(url,
+                             json={'tags': {'missing': 'srv1'}},
+                             headers={
+                                 'X-Yoki-Authorization': self.auth_header})
+        assert r.status_code == 200
+
+        d = ast.literal_eval(r.data.decode('utf-8'))
+        expected = 'Container missing not found in task definition'
+
+        assert expected in d['messages'][0]['message']
+        assert expected in d['messages'][1]['message']
+
+    def test_tag_not_found(self):
+        self.create_test_group()
+
+        url = url_for('api.ServiceGroupDeploy',
+                      cluster='test-yoki', group='group1')
+
+        r = self.client.post(url,
+                             json={'tags': {'container1': 'missing'}},
+                             headers={
+                                 'X-Yoki-Authorization': self.auth_header})
+        assert r.status_code == 200
+
+        d = ast.literal_eval(r.data.decode('utf-8'))
+        expected = 'Image not found: An error occurred'
+
+        assert expected in d['messages'][0]['message']
+        assert expected in d['messages'][1]['message']
