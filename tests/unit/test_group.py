ec52079fc1be78659100f801e577eda3ae9932f4
Borislav Ivanov <borogl@gmail.com>
2019-06-10 16:49:26 -0700

    add unit tests for service group list

--- tests/unit/test_group.py
+++ tests/unit/test_group.py
@@ -1,6 +1,6 @@
 from botocore.stub import Stubber
 
-from yoki.resources.group import ServiceGroup
+from yoki.resources.group import ServiceGroup, ServiceGroupList
 
 
 class TestServiceGroup(object):
@@ -21,3 +21,23 @@ class TestServiceGroup(object):
             r = sg.update_group('group1', {'services': ['srv1', 'srv2']})
 
         assert r == ({'message': 'Error saving item.'}, 500)
+
+    def test_delete_group(self):
+        sg = ServiceGroup()
+
+        with Stubber(sg.dynamodb.meta.client) as stubber:
+            stubber.add_client_error('delete_item')
+            r = sg.delete_group('group1')
+
+        assert r == ({'message': 'Error deleting item.'}, 500)
+
+
+class TestServiceGroupList(object):
+    def test_get_groups(self):
+        sgl = ServiceGroupList()
+
+        with Stubber(sgl.dynamodb.meta.client) as stubber:
+            stubber.add_client_error('scan')
+            r = sgl.get_groups()
+
+        assert r == ({'message': 'Error getting items.'}, 500)
